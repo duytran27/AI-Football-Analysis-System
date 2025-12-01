64c1d628cea706b01787997ba188ed8be43d85d8
duytran27 <duytran332211@gmail.com>
2025-12-01 16:05:01 -0700

    draw object trackers
    draw trackers for players, referees, ball

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 def main():
     video_frames = read_video("input_videos/test.mp4")
-    save_video(video_frames, "output_videos/output_video.avi")
+
+    #Initialize Tracker
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_objects_tracks(video_frames, read_from_stub=True, stub_path="stubs/track_stubs.pkl")
+
+    #Draw Output
+    ##Draw Object Tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 if __name__ == "__main__":
     main()
